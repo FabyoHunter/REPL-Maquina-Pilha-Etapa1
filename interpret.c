5c2c0e37b0f7014c8b36161496c87d85d22b7294
FabyoHunter <fabyohunter070705@gmail.com>
2025-06-10 15:34:50 -0300

    Update interpret.c

--- interpret.c
+++ interpret.c
@@ -1,26 +1,30 @@
 #include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdlib.h> 
+#include <stdio.h>  
 
-#include "interpret.h"
-#include "stack.h"
+#include "interpret.h" 
+#include "stack.h"     
+
 
 Stack *s;
 
-void init () {
+
+void init() {
     s = new_stack(10); 
+    printf("Máquina virtual baseada em pilha inicializada. Capacidade: 10\n");
 }
 
-void interpret (const char *source) {
-    char op[10];
-    char arg_str[10]; 
-    int num_args_read;
-    int value1, value2;
 
-   
-    num_args_read = sscanf(source, "%9s %9s", op, arg_str);
+void interpret(const char *source) {
+    char op[20];        
+    char arg_str[20];  
+    int num_args_read;  
+    int value1, value2; 
 
-    if (num_args_read == EOF) { 
+  
+    num_args_read = sscanf(source, "%19s %19s", op, arg_str);
+
+    if (num_args_read == EOF || num_args_read == 0) { 
         return;
     }
 
@@ -29,7 +33,7 @@ void interpret (const char *source) {
             fprintf(stderr, "Erro: 'push' requer um argumento numérico. Ex: push 10\n");
             return;
         }
-        stack_push(s, atoi(arg_str));
+        stack_push(s, atoi(arg_str)); 
     } else if (strcmp(op, "add") == 0) {
        
         if (s->topo < 2) {
@@ -38,7 +42,7 @@ void interpret (const char *source) {
         }
         value1 = stack_pop(s); 
         value2 = stack_pop(s); 
-        stack_push(s, value1 + value2);
+        stack_push(s, value1 + value2); 
     } else if (strcmp(op, "sub") == 0) {
         if (s->topo < 2) {
             fprintf(stderr, "Erro: 'sub' requer 2 operandos na pilha.\n");
@@ -47,7 +51,6 @@ void interpret (const char *source) {
         value1 = stack_pop(s); 
         value2 = stack_pop(s); 
         stack_push(s, value2 - value1); 
-                                       
     } else if (strcmp(op, "div") == 0) {
         if (s->topo < 2) {
             fprintf(stderr, "Erro: 'div' requer 2 operandos na pilha.\n");
@@ -55,14 +58,14 @@ void interpret (const char *source) {
         }
         value1 = stack_pop(s); 
         value2 = stack_pop(s); 
-        if (value1 == 0) { 
-            fprintf(stderr, "Erro: Divisão por zero!\n");
-            stack_push(s, value2); 
-            stack_push(s, value1); 
+        if (value1 == 0) {
+            fprintf(stderr, "Erro: Divisão por zero! Não é possível dividir por 0.\n");
+            
+            stack_push(s, value2);
+            stack_push(s, value1);
             return;
         }
-        stack_push(s, value2 / value1);
-                                       
+        stack_push(s, value2 / value1); 
     } else if (strcmp(op, "mul") == 0) {
         if (s->topo < 2) {
             fprintf(stderr, "Erro: 'mul' requer 2 operandos na pilha.\n");
@@ -76,17 +79,19 @@ void interpret (const char *source) {
             fprintf(stderr, "Erro: 'print' requer 1 operando na pilha.\n");
             return;
         }
-        value1 = stack_pop(s);
-        printf("%d\n", value1);
+        value1 = stack_pop(s); 
+        printf("Resultado: %d\n", value1);
     } else if (strcmp(op, "debug") == 0) {
         stack_print(s);
     } else if (strcmp(op, "clear") == 0) { 
         if (s != NULL) {
             s->topo = 0; 
             printf("Pilha limpa.\n");
+        } else {
+            fprintf(stderr, "Erro: Pilha não inicializada para limpar.\n");
         }
     }
-  
+   
     else {
         fprintf(stderr, "Comando desconhecido: '%s'\n", op);
     }
