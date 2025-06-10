33f32d68063e41ae364d6aba1bbde7d14639e3e8
FabyoHunter <fabyohunter070705@gmail.com>
2025-06-10 15:15:35 -0300

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,12 +1,15 @@
 #ifndef stack_h
 #define stack_h
 
-typedef struct stack Stack;
+typedef struct stack {
+    int *v;
+    int topo;
+    int max_size; // Adicionei esta linha
+} Stack; // A primeira declaração de typedef está aqui na linha 8
 
 Stack* new_stack(int size);
 void stack_push(Stack* s, int value);
 int stack_pop (Stack* s);
 void stack_print(Stack* s);
-
 
 #endif
