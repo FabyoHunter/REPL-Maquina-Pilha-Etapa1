e06111c04ec6e0471363417c6bbb5a6999d4769f
FabyoHunter <fabyohunter070705@gmail.com>
2025-06-22 16:10:59 -0300

    Update interpret.c

--- ETAPA 2/interpret.c
+++ ETAPA 2/interpret.c
@@ -19,18 +19,28 @@ void init(void) {
 }
 
 void interpret(const char *source) {
+    if (!source || source[0] == '\0') return;
+
     char op[OP_SIZE] = {0};
     char arg[ARG_SIZE] = {0};
 
-    if (!source) return;
+    arg[0] = '\0'; 
+
     sscanf(source, "%15s %31s", op, arg);
 
     if (strcmp(op, "push") == 0) {
         int value;
         if (sscanf(arg, "%d", &value) == 0) {
-            value = list_get(l, arg);
+            if (list_exist(l, arg)) {
+                value = list_get(l, arg);
+                stack_push(s, value);
+            } else {
+                printf("Erro: Variavel '%s' nao encontrada.\n", arg);
+                return; 
+            }
+        } else {
+            stack_push(s, value);
         }
-        stack_push(s, value);
     } else if (strcmp(op, "add") == 0) {
         int arg1 = stack_pop(s);
         int arg2 = stack_pop(s);
@@ -38,29 +48,38 @@ void interpret(const char *source) {
     } else if (strcmp(op, "sub") == 0) {
         int arg1 = stack_pop(s);
         int arg2 = stack_pop(s);
-        stack_push(s, arg1 - arg2);
+        stack_push(s, arg2 - arg1);
     } else if (strcmp(op, "div") == 0) {
         int arg1 = stack_pop(s);
         int arg2 = stack_pop(s);
-        if (arg2 != 0)
-            stack_push(s, arg1 / arg2);
+        if (arg1 != 0)
+            stack_push(s, arg2 / arg1);
         else
-            printf("Erro: divisão por zero\n");
+            printf("Erro: divisao por zero\n");
     } else if (strcmp(op, "mul") == 0) {
         int arg1 = stack_pop(s);
         int arg2 = stack_pop(s);
         stack_push(s, arg1 * arg2);
     } else if (strcmp(op, "print") == 0) {
-        int arg1 = stack_pop(s);
-        printf("%d\n", arg1);
+        int val_to_print = stack_pop(s);
+        printf("%d\n", val_to_print);
     } else if (strcmp(op, "debug") == 0) {
+        printf("\n--- Conteudo da Pilha ---\n");
         stack_print(s);
+        printf("\n--- Variaveis --- \n");
+        list_print(l);
     } else if (strcmp(op, "pop") == 0) {
-        int arg1 = stack_pop(s);
-        if (list_exist(l, arg)) {
-            list_set(l, arg, arg1);
-        } else {
-            list_append(l, arg, arg1);
+        int val_from_stack = stack_pop(s);
+        if (strlen(arg) > 0) { 
+            if (list_exist(l, arg)) {
+                list_set(l, arg, val_from_stack);
+            } else {
+                list_append(l, arg, val_from_stack);
+            }
         }
+    } else if (strcmp(op, "exit") == 0) {
+        // Nenhuma ação direta aqui, a saída é controlada pelo main.c
+    } else {
+        printf("Erro: Instrucao desconhecida '%s'.\n", op);
     }
 }
