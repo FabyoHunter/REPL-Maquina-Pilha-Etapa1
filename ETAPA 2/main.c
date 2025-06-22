703961cd5c261e3c236d681fe3e09540645f6fef
FabyoHunter <fabyohunter070705@gmail.com>
2025-06-22 16:15:24 -0300

    Update main.c

--- ETAPA 2/main.c
+++ ETAPA 2/main.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
+#include "interpret.h" 
 
-#include "interpret.h"
+static void repl()
+{
+  char line[1024]; 
+  for (;;)
+  {
+    printf("> ");
 
-#include "stack.h"
-#include "list.h"
-
-static void repl(void) {
-    char line[128];
-    for (;;) {
-        printf("> ");
-        if (!fgets(line, sizeof(line), stdin)) {
-            printf("\n");
-            break;
-        }
-        interpret(line);
+    if (!fgets(line, sizeof(line), stdin))
+    {
+      printf("\n"); 
+      break;       
     }
+
+    if (interpret(line) == 1) {
+        break; 
+    }
+  }
 }
 
-int main(void) {
-    init();
+int main () {
+    init(); 
     repl();
     return 0;
 }
